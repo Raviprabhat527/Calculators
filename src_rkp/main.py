3e78c4cd25213db13b21467482be1825a66c540b
Raviprabhat527 <ravikumar312004@gmail.com>
2025-04-23 21:54:55 +0530

    🐛Fix: Fix issue in calculation
    Fix issue in calculation when user use both 'Buttons' and 'keyboard' to
    input in the Entry Box. (Fix by fetching the current text from Entry Box
    before doing any operation)
    
    Why commit:
    - To fix the wrong result when user uses both 'buttons' as well
      as 'keyboard' to input (mix input)
    
    Before commit:
    - Wrong answer when user use both 'buttons' and 'keyboard' to input
    - Program evaluate only one type of data at a time. The data may be entered using either 'buttons' or 'keyboard' but not from both at same time (same time means - input box is cleared and data written on it by inputMethod1 and without clearing the input box user enteres data again using inputMethod2)
    - User can use only once input method at a time (either buttons or keyboard) for correct result
    
    After commit:
    - User can use both input methods 'buttons' and 'keyboard' at same time in
      any way and in any order
    
    Signed by:  Raviprabhat527

---
+++ src_rkp/main.py
@@ -0,0 +1,50 @@
+"""
+    Calculator (GUI) using Tkinter and Python 3.10.5
+     * author: Ravi Kumar
+     * Date: 23-04-2025 (Started)
+     * Time: 10: 52: 39 (Started)
+     * last modified: 25th april 2025
+"""
+
+from tkinter import *
+from sys import path
+from PlaceIcons import PlaceIcons
+import calculation as cal
+path[0] = "D:\\Programming\\Projects\\Python\\GUI Projects\\Calculator"
+
+
+def main():
+    """
+        Summary:
+            Main function of the program from where execution of the program starts.
+    """
+    root = Tk()  # master/root windows
+    root.title("Calculator")
+    root.geometry("570x850")
+    root.minsize(560, 760)
+    # Creating a Canvas on root then place the icons on it instead of root. So that icons will also change it's position if we resize the root window.
+    # Because when window is resized (restore down, maximize, minimize) then canvas will also resize according to it and icons placed on canvas, so by default will move with it.
+    iconsArea = Canvas(root, width=600, height=850)
+    iconsArea.create_rectangle(
+        30, 50, 540, 690, outline="#2575fc", fill=None, width=10)
+    iconsArea.pack()
+    # PlaceIcons(iconsArea, cursorType="hand2").placeIconsOnGUI()
+    PlaceIcons(iconsArea, cursorType="hand2").placeIconsOnGUI()
+
+    userText = StringVar(iconsArea, value="")
+    resultText = StringVar(iconsArea, value="")
+
+    inputArea = Entry(iconsArea, width=17, font="Calibri 30",
+                      textvariable=userText)
+    inputArea.place(x=25, y=700)
+
+    resultLabel = Label(iconsArea, width=8,
+                        font="Calibri 30", bg="black", fg="magenta", textvariable=resultText)
+    resultLabel.place(x=380, y=700)
+    cal.setIdentifiers(userText, resultText, inputArea)
+    root.mainloop()
+    # root.configure(bg="black")
+
+
+if __name__ == '__main__':
+    main()
